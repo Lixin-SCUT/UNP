abd0385d830e9911dba9995689e380a17dba9833
Lixin-ee <im.lixin@foxmail.com>
2020-01-28 18:36:43 +0800

    2020.01.28 for chapter14

---
+++ advio/dgclitimeo2.c
@@ -0,0 +1,28 @@
+//dgclitimeo2.c
+//Code by Lixin on 2020.01.28
+
+#include "unp.h"
+
+void dg_cli(FILE *fp,int sockfd,const SA *pservaddr,socklen_t servlen)
+{
+	int n;
+	char sendline[MAXLINE],recvline[MAXLINE+1];
+	struct timeval tv;
+
+	tv.tv_sec=5;
+	tv.tv_usec=0;
+	setsockopt(sockfd,SOL_SOCKET,SO_RCVTIMEO,&tv,sizeof(tv));
+	while(fgets(sendline,MAXLINE,fp)!=NULL){
+		sendto(sockfd,sendline,strlen(sendline),0,pservaddr,servlen);
+		n=recvfrom(sockfd,recvline,MAXLINE,0,NULL,NULL);
+		if(n<0){
+			if(errno==EWOULDBLOCK){
+				fprintf(stderr,"socket timeout\n");
+				continue;
+			}else
+				err_sys("recvfrom error");
+		}
+		recvline[n]=0;
+		fputs(recvline,stdout);
+	}
+}
